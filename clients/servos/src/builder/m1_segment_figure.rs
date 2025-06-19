ffd557a1838234b3f3ff452ea2d16083e3ff8a41
Rod Conan <rconan@gmto.org>
2025-06-19 15:15:35 +0000

    GMT_DOS-CLIENTS_SERVOS KEEP RBM M1 FIGURE BUILDER OPTION

--- clients/servos/src/builder/m1_segment_figure.rs
+++ clients/servos/src/builder/m1_segment_figure.rs
@@ -17,9 +17,10 @@ The M1 figure builder adds the following outputs to the FEM:
 
 Per default, the rigid body motions are removed from the mirror figures.
  **/
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct M1SegmentFigure {
     transforms: Option<Vec<na::DMatrix<f64>>>,
+    keep_rbm: bool,
 }
 
 impl M1SegmentFigure {
@@ -29,9 +30,14 @@ impl M1SegmentFigure {
     }
     /// Sets the matrices that will process the mirror figures
     ///
-    /// The removal of the rigid body motion is disabled.
+    /// The removal of the rigid body motion is applied beform the matrix transforms.
     pub fn transforms(mut self, transforms: Vec<na::DMatrix<f64>>) -> Self {
         self.transforms = Some(transforms);
+        self
+    }
+    /// Disables the removal of the rigid body motions for the mirror figures
+    pub fn keep_rigid_body_motions(mut self) -> Self {
+        self.keep_rbm = true;
         self
     }
     pub(crate) fn transforms_view<'a>(&'a mut self) -> Option<Vec<na::DMatrixView<'a, f64>>> {
@@ -52,14 +58,26 @@ impl<'a> Include<'a, M1SegmentFigure> for DiscreteStateSpace<'a, ExponentialMatr
         let Some(m1_segment_figure) = m1_segment_figure else {
             return Ok(self);
         };
-        if let Some(transforms) = m1_segment_figure.transforms_view() {
-            self.outs_with_by_name(
-                (1..=7).map(|i| format!("M1_segment_{i}_axial_d")).collect(),
-                transforms,
-            )
-        } else {
-            self.set_m1_figure_nodes()?
-                .outs_by_name((1..=7).map(|i| format!("M1_segment_{i}_axial_d")).collect())
+        // if let Some(transforms) = m1_segment_figure.transforms_view() {
+        //     self.outs_with_by_name(
+        //         (1..=7).map(|i| format!("M1_segment_{i}_axial_d")).collect(),
+        //         transforms,
+        //     )
+        // } else {
+        //     self.set_m1_figure_nodes()?
+        //         .outs_by_name((1..=7).map(|i| format!("M1_segment_{i}_axial_d")).collect())
+        // }
+        let names: Vec<_> = (1..=7).map(|i| format!("M1_segment_{i}_axial_d")).collect();
+        match (
+            m1_segment_figure.keep_rbm,
+            m1_segment_figure.transforms_view(),
+        ) {
+            (true, None) => self.outs_by_name(names),
+            (true, Some(transforms)) => self.outs_with_by_name(names, transforms),
+            (false, None) => self.set_m1_figure_nodes()?.outs_by_name(names),
+            (false, Some(transforms)) => self
+                .set_m1_figure_nodes()?
+                .outs_with_by_name(names, transforms),
         }
     }
 }
