dc1726e6b2bef2be297ae24c6bb4b3a1bdfdb73e
Rod Conan <rconan@gmto.org>
2026-02-09 01:33:38 +0000

    GMT_DOS-SYSTEMS_AGWS Kernel<Update/Read/Write>Error

--- systems/agws/src/kernels.rs
+++ systems/agws/src/kernels.rs
@@ -1,4 +1,4 @@
-use std::{any::type_name, convert::Infallible, fmt::Display, io, marker::PhantomData};
+use std::{any::type_name, fmt::Display, io, marker::PhantomData};
 
 use gmt_dos_clients_crseo::{
     OpticalModel, OpticalModelBuilder, centroiding::CentroidsError, crseo::FromBuilder,
@@ -34,26 +34,6 @@ where
             .map(|data| data.transmute::<KernelFrame<T>>())
     }
 }
-// impl<T> TryWrite<KernelFrame<T>> for OpticalModel<<T as KernelSpecs>::Sensor>
-// where
-//     T: KernelSpecs + Send + Sync,
-//     // OpticalModelBuilder<<T::Sensor as FromBuilder>::ComponentBuilder>:
-//     //     DeviceInitialize<T::Processor>,
-//     KernelFrame<T>:
-//         UniqueIdentifier<DataType = <<T as KernelSpecs>::Input as UniqueIdentifier>::DataType>,
-//     <T as KernelSpecs>::Input: UniqueIdentifier,
-//     Self: Write<<T as KernelSpecs>::Input>,
-// {
-//     type Error = Infallible;
-
-//     fn try_write(
-//         &mut self,
-//     ) -> std::result::Result<Option<Data<KernelFrame<T>>>, <Self as TryWrite<KernelFrame<T>>>::Error>
-//     {
-//         Ok(<Self as Write<<T as KernelSpecs>::Input>>::write(self)
-//             .map(|data| data.transmute::<KernelFrame<T>>()))
-//     }
-// }
 
 #[derive(Debug, thiserror::Error)]
 pub enum KernelError {
@@ -160,11 +140,28 @@ where
 //     }
 // }
 
+#[derive(Debug)]
+pub struct KernelReadError(Box<dyn std::error::Error + Send + Sync>);
+impl Display for KernelReadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Kernel failed to read data caused by")?;
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+impl std::error::Error for KernelReadError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(self.0.as_ref())
+    }
+}
+impl From<Box<dyn std::error::Error + Send + Sync>> for KernelReadError {
+    fn from(value: Box<dyn std::error::Error + Send + Sync>) -> Self {
+        KernelReadError(value)
+    }
+}
+
 impl<T> TryRead<KernelFrame<T>> for Kernel<T>
 where
     T: KernelSpecs,
-    // OpticalModelBuilder<<T::Sensor as FromBuilder>::ComponentBuilder>:
-    //     DeviceInitialize<T::Processor>,
     KernelFrame<T>:
         UniqueIdentifier<DataType = <<T as KernelSpecs>::Input as UniqueIdentifier>::DataType>,
     <T as KernelSpecs>::Input: UniqueIdentifier,
@@ -175,68 +172,119 @@ where
     <T as KernelSpecs>::Output: UniqueIdentifier,
     <T as KernelSpecs>::Estimator: TryWrite<<T as KernelSpecs>::Output>,
     <T as KernelSpecs>::Integrator: TryRead<<T as KernelSpecs>::Output>,
+    <<T as KernelSpecs>::Processor as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Processor as TryWrite<<T as KernelSpecs>::Data>>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryRead<<T as KernelSpecs>::Data>>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryWrite<<T as KernelSpecs>::Output>>::Error: 'static,
+    <<T as KernelSpecs>::Integrator as TryRead<<T as KernelSpecs>::Output>>::Error: 'static,
+    <<T as KernelSpecs>::Integrator as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Processor as TryRead<<T as KernelSpecs>::Input>>::Error: 'static,
 {
-    type Error = KernelError;
+    type Error = KernelReadError;
 
     fn try_read(
         &mut self,
         data: Data<KernelFrame<T>>,
     ) -> std::result::Result<&mut Self, <Self as TryRead<KernelFrame<T>>>::Error> {
-        <<T as KernelSpecs>::Processor as TryRead<<T as KernelSpecs>::Input>>::try_read(
+        <<T as KernelSpecs>::Processor as TryRead<<T as KernelSpecs>::Input>>::boxed_try_read(
             &mut self.processor,
             data.transmute::<<T as KernelSpecs>::Input>(),
-        )
-        .unwrap();
+        )?;
         Ok(self)
     }
 }
+
+#[derive(Debug)]
+pub struct KernelUpdateError(Box<dyn std::error::Error + Send + Sync>);
+impl Display for KernelUpdateError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Kernel failed to update cause by")?;
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+impl std::error::Error for KernelUpdateError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(self.0.as_ref())
+    }
+}
+impl From<Box<dyn std::error::Error + Send + Sync>> for KernelUpdateError {
+    fn from(value: Box<dyn std::error::Error + Send + Sync>) -> Self {
+        KernelUpdateError(value)
+    }
+}
+
 impl<T> TryUpdate for Kernel<T>
 where
     T: KernelSpecs,
-    // OpticalModelBuilder<<T::Sensor as FromBuilder>::ComponentBuilder>:
-    //     DeviceInitialize<T::Processor>,
     <T as KernelSpecs>::Data: UniqueIdentifier,
     <T as KernelSpecs>::Processor: TryWrite<<T as KernelSpecs>::Data>,
     <T as KernelSpecs>::Estimator: TryRead<<T as KernelSpecs>::Data>,
     <T as KernelSpecs>::Output: UniqueIdentifier,
     <T as KernelSpecs>::Estimator: TryWrite<<T as KernelSpecs>::Output>,
     <T as KernelSpecs>::Integrator: TryRead<<T as KernelSpecs>::Output>,
+    <<T as KernelSpecs>::Processor as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Processor as TryWrite<<T as KernelSpecs>::Data>>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryRead<<T as KernelSpecs>::Data>>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryWrite<<T as KernelSpecs>::Output>>::Error: 'static,
+    <<T as KernelSpecs>::Integrator as TryRead<<T as KernelSpecs>::Output>>::Error: 'static,
+    <<T as KernelSpecs>::Integrator as TryUpdate>::Error: 'static,
 {
-    type Error = Infallible;
+    type Error = KernelUpdateError;
 
     fn try_update(&mut self) -> std::result::Result<&mut Self, Self::Error> {
         log::info!("updating kernel: {}", type_name::<T>());
-        self.processor.try_update().unwrap();
-        self.estimator.as_mut().map(|estimator| {
-            <<T as KernelSpecs>::Processor as TryWrite<<T as KernelSpecs>::Data>>::try_write(
-                &mut self.processor,
-            ).unwrap()
-            .map(|data| {
-                <<T as KernelSpecs>::Estimator as TryRead<<T as KernelSpecs>::Data>>::try_read(
+        self.processor.boxed_try_update()?;
+        if let Some(estimator) = self.estimator.as_mut() {
+            if let Some(data) = <<T as KernelSpecs>::Processor as TryWrite<
+                <T as KernelSpecs>::Data,
+            >>::boxed_try_write(&mut self.processor)?
+            {
+                <<T as KernelSpecs>::Estimator as TryRead<<T as KernelSpecs>::Data>>::boxed_try_read(
                     estimator, data,
-                ).unwrap();
-                estimator.try_update().unwrap();
-            });
-            self.integrator.as_mut().map(|integrator| {
-                <<T as KernelSpecs>::Estimator as TryWrite<<T as KernelSpecs>::Output>>::try_write(
-                    estimator,
-                ).unwrap()
-                .map(|data| {
-                    <<T as KernelSpecs>::Integrator as TryRead<<T as KernelSpecs>::Output>>::try_read(
+                )
+                    ?;
+                estimator.boxed_try_update()?;
+            };
+            if let Some(integrator) = self.integrator.as_mut() {
+                if let Some(data) = <<T as KernelSpecs>::Estimator as TryWrite<
+                    <T as KernelSpecs>::Output,
+                >>::boxed_try_write(estimator)?
+                {
+                    <<T as KernelSpecs>::Integrator as TryRead<<T as KernelSpecs>::Output>>::boxed_try_read(
                         integrator, data,
-                    ).unwrap();
-                    integrator.try_update().unwrap();
-                });
-            });
-        });
+                    )?;
+                    integrator.boxed_try_update()?;
+                };
+            };
+        };
         Ok(self)
     }
 }
+
+#[derive(Debug)]
+pub struct KernelWriteError(Box<dyn std::error::Error + Send + Sync>);
+impl Display for KernelWriteError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Kernel failed to write data caused by")?;
+        std::fmt::Display::fmt(&self.0, f)
+    }
+}
+impl std::error::Error for KernelWriteError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(self.0.as_ref())
+    }
+}
+impl From<Box<dyn std::error::Error + Send + Sync>> for KernelWriteError {
+    fn from(value: Box<dyn std::error::Error + Send + Sync>) -> Self {
+        KernelWriteError(value)
+    }
+}
+
 impl<T> TryWrite<<T as KernelSpecs>::Output> for Kernel<T>
 where
     T: KernelSpecs,
-    // OpticalModelBuilder<<T::Sensor as FromBuilder>::ComponentBuilder>:
-    //     DeviceInitialize<T::Processor>,
     <T as KernelSpecs>::Output: UniqueIdentifier,
     <T as KernelSpecs>::Integrator: TryWrite<<T as KernelSpecs>::Output>,
     <T as KernelSpecs>::Data: UniqueIdentifier,
@@ -245,8 +293,16 @@ where
     <T as KernelSpecs>::Output: UniqueIdentifier,
     <T as KernelSpecs>::Estimator: TryWrite<<T as KernelSpecs>::Output>,
     <T as KernelSpecs>::Integrator: TryRead<<T as KernelSpecs>::Output>,
+    <<T as KernelSpecs>::Processor as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Processor as TryWrite<<T as KernelSpecs>::Data>>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryRead<<T as KernelSpecs>::Data>>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Estimator as TryWrite<<T as KernelSpecs>::Output>>::Error: 'static,
+    <<T as KernelSpecs>::Integrator as TryRead<<T as KernelSpecs>::Output>>::Error: 'static,
+    <<T as KernelSpecs>::Integrator as TryUpdate>::Error: 'static,
+    <<T as KernelSpecs>::Integrator as TryWrite<<T as KernelSpecs>::Output>>::Error: 'static,
 {
-    type Error = Infallible;
+    type Error = KernelWriteError;
 
     fn try_write(
         &mut self,
@@ -255,10 +311,10 @@ where
         <Self as TryWrite<<T as KernelSpecs>::Output>>::Error,
     > {
         Ok(if let Some(integrator) = self.integrator.as_mut() {
-            <<T as KernelSpecs>::Integrator as TryWrite<_>>::try_write(integrator).unwrap()
+            <<T as KernelSpecs>::Integrator as TryWrite<_>>::boxed_try_write(integrator)?
         } else {
             if let Some(estimator) = self.estimator.as_mut() {
-                <<T as KernelSpecs>::Estimator as TryWrite<_>>::try_write(estimator).unwrap()
+                <<T as KernelSpecs>::Estimator as TryWrite<_>>::boxed_try_write(estimator)?
             } else {
                 None
             }
