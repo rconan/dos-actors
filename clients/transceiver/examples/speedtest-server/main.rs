67ce67c89fb3954af9f296510ad272fcf7dbb0fd
Rod Conan <rconan@gmto.org>
2026-02-20 17:33:14 +0000

    GMT_DOS-CLIENTS_TRANSCEIVER Updated speedtest server

--- clients/transceiver/examples/speedtest-server/main.rs
+++ clients/transceiver/examples/speedtest-server/main.rs
@@ -42,14 +42,26 @@ impl Write<Packet> for Payload {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .finish(),
+    )?;
     let mut args = env::args();
     args.next();
-    let len = args.next().and_then(|v| v.parse::<usize>().ok()).unwrap();
-    let repeat = args.next().and_then(|v| v.parse::<usize>().ok()).unwrap();
+    let len = args
+        .next()
+        .map_or_else(|| Ok(0), |v| v.parse::<u32>())
+        .map(|x| 2usize.pow(x))?;
+    let repeat = args
+        .next()
+        .map_or_else(|| Ok(0), |v| v.parse::<u32>())
+        .map(|x| 10usize.pow(x))?;
+    println!("Sending a {} bytes data packet {} times", len, repeat);
 
     let mut payload: Initiator<_> = Payload::new(len, repeat).into();
     let mut monitor = Monitor::new();
-    let mut tx: Terminator<_> = Transceiver::<Packet>::transmitter("172.31.26.127:5001")?
+    let mut tx: Terminator<_> = Transceiver::<Packet>::transmitter("172.31.11.38")?
         .run(&mut monitor)
         .into();
 
