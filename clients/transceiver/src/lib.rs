4029ff0dd981126022db4fbe06276e8c199ed79a
Rod Conan <rconan@gmto.org>
2026-02-24 16:54:41 +0000

    GMT_DOS-CLIENTS_TRANSCEIVER remove Optics trait impl

--- clients/transceiver/src/lib.rs
+++ clients/transceiver/src/lib.rs
@@ -22,7 +22,7 @@ mod transmitter;
 
 use std::{any::type_name, marker::PhantomData};
 
-use interface::{Data, Read, UniqueIdentifier, Update, Write, optics::Optics};
+use interface::{Data, Read, UniqueIdentifier, Update, Write};
 use quinn::Endpoint;
 
 pub use crypto::Crypto;
@@ -183,8 +183,6 @@ impl<U: UniqueIdentifier, F, S> std::fmt::Display for Transceiver<U, F, S> {
         Ok(())
     }
 }
-
-impl<U: UniqueIdentifier, F> Optics for Transceiver<U, F, On> {}
 
 /* impl<U: UniqueIdentifier, V: UniqueIdentifier, F> From<&Transceiver<U, F>> for Transceiver<V, F> {
     fn from(other: &Transceiver<U, F>) -> Self {
