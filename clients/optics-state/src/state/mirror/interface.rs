a18009a529890292f0a5cea9b478ee6083fb38d5
Rod Conan <rconan@gmto.org>
2026-03-02 15:45:41 +0000

    GMT_DOS-CLIENTS_OPTICS_STATE impl Write<RBM> for MirrorState

--- clients/optics-state/src/state/mirror/interface.rs
+++ clients/optics-state/src/state/mirror/interface.rs
@@ -1,9 +1,28 @@
-use gmt_dos_clients_io::{gmt_m1::M1RigidBodyMotions, gmt_m2::M2RigidBodyMotions};
+use std::mem;
+
+use gmt_dos_clients_io::{
+    gmt_m1::{self, M1RigidBodyMotions},
+    gmt_m2::{self, M2RigidBodyMotions},
+};
 use interface::{Data, Read, Update, Write};
 
 use crate::{M1State, M2State, MirrorState, SegmentState};
 
 impl Update for MirrorState {}
+
+impl Read<M1State> for MirrorState {
+    fn read(&mut self, data: Data<M1State>) {
+        let state = &*data;
+        let _ = mem::replace(self, state.clone());
+    }
+}
+
+impl Read<M2State> for MirrorState {
+    fn read(&mut self, data: Data<M2State>) {
+        let state = &*data;
+        let _ = mem::replace(self, state.clone());
+    }
+}
 
 impl Read<M2RigidBodyMotions> for MirrorState {
     fn read(&mut self, data: Data<M2RigidBodyMotions>) {
@@ -12,12 +31,6 @@ impl Read<M2RigidBodyMotions> for MirrorState {
             .map(|data| SegmentState::rbms(data))
             .map(|segment| Some(segment))
             .collect();
-    }
-}
-
-impl Write<M2State> for MirrorState {
-    fn write(&mut self) -> Option<Data<M2State>> {
-        Some(Data::new(self.clone()))
     }
 }
 
@@ -31,8 +44,32 @@ impl Read<M1RigidBodyMotions> for MirrorState {
     }
 }
 
+impl<const ID: u8> Write<gmt_m1::segment::RBM<ID>> for MirrorState {
+    fn write(&mut self) -> Option<Data<gmt_m1::segment::RBM<ID>>> {
+        Some(
+            self.iter()
+                .nth(ID as usize - 1)
+                .flatten()
+                .map_or_else(
+                    || vec![0f64; 6],
+                    |SegmentState { rbms, .. }| {
+                        rbms.as_ref()
+                            .map_or_else(|| vec![0f64; 6], |rbms| rbms.as_ref().to_vec())
+                    },
+                )
+                .into(),
+        )
+    }
+}
+
 impl Write<M1State> for MirrorState {
     fn write(&mut self) -> Option<Data<M1State>> {
+        Some(Data::new(self.clone()))
+    }
+}
+
+impl Write<M2State> for MirrorState {
+    fn write(&mut self) -> Option<Data<M2State>> {
         Some(Data::new(self.clone()))
     }
 }
