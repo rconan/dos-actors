784f1b5d1605a1a19c74ae8f7a516383274ba646
Rod Conan <rconan@gmto.org>
2026-03-18 17:26:27 +0000

    GMT_DOS-CLIENTS low-pass-filter corner frequency

--- clients/core/src/low_pass_filter.rs
+++ clients/core/src/low_pass_filter.rs
@@ -1,5 +1,6 @@
 use std::{
-    fmt::Debug,
+    f64,
+    fmt::{Debug, Display, LowerExp},
     ops::{Add, AddAssign, Mul, Sub},
     sync::Arc,
 };
@@ -13,13 +14,32 @@ pub struct LowPassFilter<T> {
     g: T,
 }
 
+impl<T: Default + Clone + Display + LowerExp> Display for LowPassFilter<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "low-pass-filer: n={} , g={:.4e}", self.y.len(), self.g)
+    }
+}
+
 impl<T: Default + Clone> LowPassFilter<T> {
+    /// Creates a new low-pass filter instance from the data size and the gain
     pub fn new(n: usize, g: T) -> Self {
         Self {
             u: Arc::new(vec![T::default(); n]),
             y: vec![T::default(); n],
             g,
         }
+    }
+}
+impl LowPassFilter<f64> {
+    /// Creates a new low-pass filter instance from the data size,
+    ///  the corner and sampling frequencies in Hz
+    pub fn from_corner_frequency(n: usize, corner: f64, sampling: f64) -> Self {
+        let g = 1f64 - (-2f64 * f64::consts::PI * corner / sampling).exp();
+        Self::new(n, g)
+    }
+    /// Returns the corner frequency for the given sampling
+    pub fn corner_frequency(&self, sampling: f64) -> f64 {
+        -0.5 * sampling * f64::consts::FRAC_1_PI * (1f64 - self.g).ln()
     }
 }
 
@@ -56,5 +76,20 @@ where
 {
     fn write(&mut self) -> Option<Data<U>> {
         Some(self.y.clone().into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::f64;
+
+    use super::*;
+
+    #[test]
+    fn corner_frequency() {
+        let lpf = LowPassFilter::from_corner_frequency(3, 1f64, 1e3);
+        println!("{lpf}");
+        let cf = lpf.corner_frequency(1e3);
+        assert!((1. - cf).abs() < f64::EPSILON * 1e3);
     }
 }
