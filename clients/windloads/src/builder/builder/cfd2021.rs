39ba3462e29bda64514c0bb0153dc2cf2306d977
Rod Conan <rconan@gmto.org>
2026-04-24 21:27:03 +0000

    GMT_DOS-CLIENTS_WINDLOADS Update APIs

--- clients/windloads/src/builder/builder/cfd2021.rs
+++ clients/windloads/src/builder/builder/cfd2021.rs
@@ -137,8 +137,8 @@ impl<S> Builder<S> {
                                 Into::<Option<[f64; 3]>>::into(&exertion[i].force),
                                 Into::<Option<[f64; 3]>>::into(&exertion[i].moment),
                             ) {
-                                exertion[i].force = f.vfrov(M1S::new(*j))?.into();
-                                exertion[i].moment = m.vfrov(M1S::new(*j))?.into();
+                                exertion[i].force = f.vfrov(M1S::new(*j)?).into();
+                                exertion[i].moment = m.vfrov(M1S::new(*j)?).into();
                             };
                             if let Some(m1_fm) = m1_fm.as_mut() {
                                 m1_fm.push((&exertion[i]).into());
@@ -153,8 +153,8 @@ impl<S> Builder<S> {
                                 Into::<Option<[f64; 3]>>::into(&exertion[i].force),
                                 Into::<Option<[f64; 3]>>::into(&exertion[i].moment),
                             ) {
-                                exertion[i].force = f.vfrov(M2S::new(*j))?.into();
-                                exertion[i].moment = m.vfrov(M2S::new(*j))?.into();
+                                exertion[i].force = f.vfrov(M2S::new(*j)?).into();
+                                exertion[i].moment = m.vfrov(M2S::new(*j)?).into();
                             };
                             if let Some(m2_fm) = m2_fm.as_mut() {
                                 m2_fm.push((&exertion[i]).into());
